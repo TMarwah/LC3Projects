a5746f7f1d7a629df7e79d601e134d3d7ebb2ccc
Tanmay Marwah <marwah.tanmay@gmail.com>
2023-01-31 18:27:19 -0800

    ex3 done

--- lab4/lab4_ex3.asm
+++ lab4/lab4_ex3.asm
@@ -1,9 +1,57 @@
 ;=================================================
-; Name: 
-; Email: 
+; Name: Tanmay Marwah
+; Email: tmarw001@ucr.edu
 ; 
 ; Lab: lab 4, ex 3
-; Lab section: 
-; TA: 
+; Lab section: 23
+; TA: Westin Montano and Omer Eren
 ; 
 ;=================================================
+
+.ORIG x3000
+
+;LOAD R1 with array starting address
+LD R1, ARRAY_PTR
+
+;LOAD loop counter
+LD R2, DEC_10
+
+;zero out R0
+AND R0, R0, x0
+LD R0, DEC_1
+
+
+;========================
+;LOAD ARRAY WITH VALS 0-9
+;========================
+DO_WHILE_LOOP
+    STR R0, R1, #0
+    ADD R1, R1, #1
+    ADD R0, R0, R0
+    ADD R2, R2, #-1 ;iterate loop
+    BRp DO_WHILE_LOOP
+END_DO_WHILE_LOOP
+
+
+AND R1, R1, x0
+LD R1, ARRAY_PTR
+
+AND R2, R2, x0
+LDR R2, R1, #6
+
+
+
+
+
+HALT
+ARRAY_PTR .FILL x4000
+DEC_10 .FILL #10
+DEC_1 .FILL #1
+.END
+
+
+
+
+.ORIG x4000
+ARRAY .BLKW #10
+.END
