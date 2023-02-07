3e8d5b9c98d7dc95087abf0d94d52aa1238ac137
Tanmay Marwah <marwah.tanmay@gmail.com>
2023-02-06 17:12:23 -0800

    completed prog assn 2

--- assn3/assignment3.asm
+++ assn3/assignment3.asm
@@ -20,7 +20,8 @@
 LD R6, Value_ptr		; R6 <-- pointer to value to be displayed as binary
 LD R4, LOOP_COUNT
 LD R7, PACKET_COUNT
-LDR R1, R6, #0			; R1 <-- value to be displayed as binary 
+LD R3, PACKET_NUMS
+LDR R1, R6, #0  		; R1 <-- value to be displayed as binary 
 
 ;-------------------------------
 ;INSERT CODE STARTING FROM HERE
@@ -28,38 +29,46 @@ LDR R1, R6, #0			; R1 <-- value to be displayed as binary
 
 
 WHILE
-    ADD R1, R1, #0
+    ADD R1, R1, #0 ; make sure to use r1 as last modified reg
     
-    BRp BIT_IS_ONE
+    BRn BIT_IS_ONE ;if num is negative, two's comp says msb must be a 1
     
     ;BIT IS 0
-    LD R0, ASCII0
+    LD R0, ASCII0 ; if not neg, msb is 0 according 
     OUT
     ADD R7,R7, #-1
     BRp SKIP
+    BRz SKIP
     
     ;BIT IS 1
     BIT_IS_ONE
-    LD R0, ASCII1
-    ADD R7,R7, #-1
-    OUT
+        LD R0, ASCII1
+        ADD R7,R7, #-1
+        OUT
     
     SKIP
+    ADD R3, R3, #0 ; change last mod reg to checking if at last packet or not
+    BRz PACKET_NUMS_SKIP ;if 0, means we are at the last packet and do not need space
+    ADD R7, R7, #0 ; change last mod reg to checking if 4 bits have been printed or not
     BRz PACKET_SPACE ;counter is done
     BRp PACKET_SKIP ;counter is not done
     
-    PACKET_SPACE
+    PACKET_SPACE ; print space between 4 bit packets
+        AND R0,R0,x0
         LD R0, SPACE
         OUT 
+        ADD R3, R3, #-1
         LD R7, PACKET_COUNT
     
-    PACKET_SKIP
+   PACKET_SKIP ; skip if packet is not done
+   PACKET_NUMS_SKIP ; skip if last packet is reached, space is not needed
+
     ADD R1, R1, R1
     ADD R4,R4,#-1
     BRp WHILE
 END_WHILE
 
-LD R0, SPACE
+LD R0, NEWLINE
 OUT
 
     
@@ -74,6 +83,8 @@ ASCII1 .fill x31
 LOOP_COUNT .fill #16
 PACKET_COUNT .fill #4
 SPACE .fill x20
+NEWLINE .fill x0A
+PACKET_NUMS .fill #3
 .END
 
 .ORIG xCA01					; Remote data
