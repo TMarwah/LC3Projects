f7cba8af505d9eea53edb518fb8513d528055144
Tanmay Marwah <marwah.tanmay@gmail.com>
2023-03-06 16:13:28 -0800

    complete ex1

--- lab8/lab8_ex1.asm
+++ lab8/lab8_ex1.asm
@@ -22,8 +22,6 @@ AND R7, R7, x0
 
 LD R6, top_stack_addr
 
-; Test harness
-;-------------------------------------------------
 LD R5, LOAD_FILL_VALUE_3200
 JSRR R5
 
@@ -32,6 +30,11 @@ ADD R1, R1, #1
 
 LD R5, OUTPUT_AS_DECIMAL_3400
 JSRR R5
+
+
+
+; Test harness
+;-------------------------------------------------
 
 HALT
 
@@ -64,6 +67,7 @@ LDR R7, R6, #0
 ADD R6, R6, #1
 
 RET
+
 ;hardcoded value
 VAL_TO_CONVERT .fill #32767
 .end
@@ -72,7 +76,7 @@ VAL_TO_CONVERT .fill #32767
 ; Subroutine: OUTPUT_AS_DECIMAL_3400
 ; Parameter: (R1): Value that must be converted
 ; Postcondition: Print new value as decimal number
-; Return Value: Decimal number to terminal
+; Return Value: l
 ;=================================================
 
 .orig x3400
@@ -80,12 +84,14 @@ VAL_TO_CONVERT .fill #32767
 ; Backup registers
 ADD R6, R6, #-1
 STR R7, R6, #0
+ADD R6, R6, #-1
+STR R4, R6, #0
+ADD R6, R6, #-1
+STR R3, R6, #0
+ADD R6, R6, #-1
+STR R2, R6, #0
 
-; Code
-AND R2, R2, x0
-ADD R2, R2, R1
-AND R4, R4, x0
-
+; Print minus if neg, otherwise skip
 ADD R1, R1, #0
 BRp PRINT_POSITIVE
 LD R0, MINUS_SIGN
@@ -95,50 +101,109 @@ BR SKIP_POSITIVE
 PRINT_POSITIVE
 SKIP_POSITIVE
 
-;TEN THOUSANDTHS PLACE
-ten_thousandths_place
-    LD R3, ten_thousandths
-    ADD R2, R2, R3
-    BRn tthousandths_place_complete
-    ADD R4, R4, #1
-BR ten_thousandths_place
-tthousandths_place_complete
-ADD R2, R2, R3
+LD R4, ASCII_OFFSET
 
-LD R5 ASCII_OFFSET
-ADD R4, R4, R5
+AND R3, R3, x0
+AND R2, R2, x0
+COUNT_TEN_THOUSANDTHS
+    LD R2, ten_thousandths
+    ADD R1, R1, R2
+    BRn TEN_THOUSANDTHS_DONE
+    ADD R3, R3, #1
+    BR COUNT_TEN_THOUSANDTHS
+
+TEN_THOUSANDTHS_DONE
+NOT R2, R2
+ADD R2, R2, #1
+ADD R1, R2, R1
+
 AND R0, R0, x0
+ADD R0, R3, #0
 ADD R0, R0, R4
+
 OUT
 
-AND R4, R4, x0
 AND R3, R3, x0
-;THOUSANDTHS PLACE
-thousandths_place
-    LD R3, thousandths
-    ADD R2, R2, R3
-    BRn thousandths_place_complete
-    ADD R4, R4, #1
-BR thousandths_place
-thousandths_place_complete
-ADD R2, R2, R3
+AND R2, R2, x0
+COUNT_THOUSANDTHS
+    LD R2, thousandths
+    ADD R1, R1, R2
+    BRn THOUSANDTHS_DONE
+    ADD R3, R3, #1
+    BR COUNT_THOUSANDTHS
 
-ADD R4, R4, R5
+THOUSANDTHS_DONE
+NOT R2, R2
+ADD R2, R2, #1
+ADD R1, R2, R1
+
 AND R0, R0, x0
+ADD R0, R3, #0
+ADD R0, R0, R4
+
+OUT
+
+AND R3, R3, x0
+AND R2, R2, x0
+COUNT_HUNDERETHS
+    LD R2, hundereths
+    ADD R1, R1, R2
+    BRn HUNDERETHS_DONE
+    ADD R3, R3, #1
+    BR COUNT_HUNDERETHS
+
+HUNDERETHS_DONE
+NOT R2, R2
+ADD R2, R2, #1
+ADD R1, R2, R1
+
+AND R0, R0, x0
+ADD R0, R3, #0
+ADD R0, R0, R4
+
+OUT
+
+AND R3, R3, x0
+AND R2, R2, x0
+COUNT_TENS
+    LD R2, tens
+    ADD R1, R1, R2
+    BRn TENS_DONE
+    ADD R3, R3, #1
+    BR COUNT_TENS
+
+TENS_DONE
+NOT R2, R2
+ADD R2, R2, #1
+ADD R1, R2, R1
+
+AND R0, R0, x0
+ADD R0, R3, #0
+ADD R0, R0, R4
+
+OUT
+
+;PRINT ONES
+AND R0, R0, x0
+ADD R0, R1, #0
 ADD R0, R0, R4
 OUT
 
 ; Restore registers
+LDR R2, R6, #0
+ADD R6, R6, #1
+LDR R3, R6, #0
+ADD R6, R6, #1
+LDR R4, R6, #0
+ADD R6, R6, #1
 LDR R7, R6, #0
 ADD R6, R6, #1
-
 RET
-ASCII_OFFSET .FILL x30
 MINUS_SIGN .FILL x2D
-ten_thousandths .FILL #-10000 
+ten_thousandths .FILL #-10000
 thousandths .FILL #-1000
 hundereths .FILL #-100
-tenths .FILL #-10
-ones .FILL #1
-
+tens .FILL #-10
+ones .FILL #-1
+ASCII_OFFSET .FILL x30
 .end
